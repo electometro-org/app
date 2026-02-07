e0938ec1aaf68d4cf38492a24a4bda6640656e1a
seffs <46768740+seffs@users.noreply.github.com>
2026-02-07 16:22:05 +0100

    fix: remove placeholder bar (peru election)

--- src/elections/peru_2026.js
+++ src/elections/peru_2026.js
@@ -172,13 +172,13 @@ export default {
     //   resizable: true,
     //   layouts: { lg: { x: 0, y: 0, w: 96, h: 8 } }
     // },
-    {
-      type: "placeholder",
-      id: "test-sidebar",
-      label: "",
-      layouts: { lg: { x: 0, y: 10, w: 20, h: 40 } },
-      invisible: false,
-    },
+    // {
+    //   type: "placeholder",
+    //   id: "test-sidebar",
+    //   label: "",
+    //   layouts: { lg: { x: 0, y: 10, w: 20, h: 40 } },
+    //   invisible: false,
+    // },
     // {
     //   type: "placeholder",
     //   id: "button-overlay",

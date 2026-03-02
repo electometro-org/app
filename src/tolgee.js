9806053b0c01c882b5d198d611ddf48e75735fd3
seffs <46768740+seffs@users.noreply.github.com>
2026-03-02 08:30:20 +0100

    feat: add fallback logic for translation files in case of CDN failure

--- src/tolgee.js
+++ src/tolgee.js
@@ -1,13 +1,32 @@
 import { Tolgee, DevTools, FormatSimple, BackendFetch } from "@tolgee/react";
 
+
+const getStaticData = {
+    es: () => import("../i18n/es.json").then(m => m.default),
+    qu: () => import("../i18n/qu.json").then(m => m.default),
+    ay: () => import("../i18n/ay.json").then(m => m.default),
+};
+
 export const tolgee = Tolgee()
   .use(DevTools())
   .use(FormatSimple())
-  .use(BackendFetch({ prefix: '/ab1c998a78a47994cdf7e70f93bc5e9c'}))
+  .use(BackendFetch({
+      prefix: '/ab1c998a78a47994cdf7e70f93bc5e9c',
+      fallbackOnFail: true,
+      timeout: 5000,
+      getData: async response => {
+          if (!response.ok) {
+              console.warn('Tolgee: Network response was not ok, using static translations');
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      }
+  }))
   .init({
     apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
     apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
     defaultLanguage: 'es',
     availableLanguages: ['es', 'qu', 'ay'],
     fallbackLanguage: 'es',
+    staticData: getStaticData
   });

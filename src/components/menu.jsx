aa68f91269e4090d2d2dcd6b278a5771f5e09dec
seffs <46768740+seffs@users.noreply.github.com>
2026-03-14 10:35:10 +0100

    fix: close menu when clicking/tapping outside of it

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -6,14 +6,17 @@ import { T } from "@tolgee/react";
 export default function Menu({ open, onClose }) {
   if (!open) return null;
   return (
-    <div className="menu-panel">
-      <ul style={{ textDecoration: "none", listStyle: "none", margin: 0, padding: 10 }}>
-        <li className="menu-list-item"><Link to="/" onClick={onClose}><T keyName="nav.survey" /></Link></li>
-        <li className="menu-list-item"><Link to="/metodologia" onClick={onClose}><T keyName="nav.methodology" /></Link></li>
-        <li className="menu-list-item"><Link to="/contacto" onClick={onClose}><T keyName="nav.contact" /></Link></li>
-        <li className="menu-list-item"><Link to="/politica-privacidad" onClick={onClose}><T keyName="nav.privacy" /></Link></li>
-        <li className="menu-list-item"><Link to="/configuracion-privacidad" onClick={onClose}><T keyName="nav.privacySettings" /></Link></li>
-      </ul>
-    </div>
+    <>
+      <div className="menu-backdrop" onClick={onClose} />
+      <div className="menu-panel">
+        <ul style={{ textDecoration: "none", listStyle: "none", margin: 0, padding: 10 }}>
+          <li className="menu-list-item"><Link to="/" onClick={onClose}><T keyName="nav.survey" /></Link></li>
+          <li className="menu-list-item"><Link to="/metodologia" onClick={onClose}><T keyName="nav.methodology" /></Link></li>
+          <li className="menu-list-item"><Link to="/contacto" onClick={onClose}><T keyName="nav.contact" /></Link></li>
+          <li className="menu-list-item"><Link to="/politica-privacidad" onClick={onClose}><T keyName="nav.privacy" /></Link></li>
+          <li className="menu-list-item"><Link to="/configuracion-privacidad" onClick={onClose}><T keyName="nav.privacySettings" /></Link></li>
+        </ul>
+      </div>
+    </>
   );
 }
